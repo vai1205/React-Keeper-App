7c8b175dc016d6a973b7cd7f262a400fc611c8e0
vai1205 <vaibhav.12may95@gmail.com>
2019-11-29 06:17:11 +0530

    add header, footer, note and app components to the app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function footer (){
+    const currentYear = new Date().getFullYear()
+    return(
+        <footer>
+            <p>Copyright © {currentYear}</p>
+        </footer>
+    )
+}
+export default footer
